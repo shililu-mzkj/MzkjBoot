6deac2dfa8a19dac9f4817e7ad5569d4ca7322b8
石理科技 <lumw_sina@163.com>
2019-11-13 17:46:44 +0800

    框架层修改-版本号管理

--- mzkj-boot-module-system/src/main/java/org/mzkj/boot/system/MzkjBootApplication.java
+++ mzkj-boot-module-system/src/main/java/org/mzkj/boot/system/MzkjBootApplication.java
@@ -11,7 +11,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 /**
- * @title bd启动类
+ * @title 启动类
  * @description 
  * @author shililu
  * @updateTime 2019-10-18 18:12
@@ -20,16 +20,16 @@ import java.net.UnknownHostException;
 @Slf4j
 @EnableSwagger2
 @SpringBootApplication
-public class BDApplication {
+public class MzkjBootApplication {
 
     public static void main(String[] args) throws UnknownHostException {
-        ConfigurableApplicationContext application = SpringApplication.run(BDApplication.class, args);
+        ConfigurableApplicationContext application = SpringApplication.run(MzkjBootApplication.class, args);
         Environment env = application.getEnvironment();
         String ip = InetAddress.getLocalHost().getHostAddress();
         String port = env.getProperty("server.port");
         String path = env.getProperty("server.servlet.context-path");
         log.info("\n----------------------------------------------------------\n\t" +
-                "Application bdpolicy-service is running! Access URLs:\n\t" +
+                "Application mzkj-service is running! Access URLs:\n\t" +
                 "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
                 "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
                 "swagger-ui: \thttp://" + ip + ":" + port + path + "/swagger-ui.html\n\t" +
